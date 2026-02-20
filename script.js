59aae8e9f4c12819da936c9cb26d3ebfd91af7e8
Mihirkumar24 <97471016+Mihirkumar24@users.noreply.github.com>
2026-02-20 12:36:17 +0530

    Refactor weather fetching and display logic
    Refactored weather fetching logic to use a single API key and base URL. Updated temperature display to include units.

--- script.js
+++ script.js
@@ -214,32 +214,32 @@
 //   // };
 //   // }
 
-
 const options = {
   method: "GET",
 };
+
+const API_KEY = "68c8998b8fa0f2a3b5af243607a8a3b3";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 const getWeather = (city) => {
   cityName.innerHTML = city;
 
   // Dynamic city (search input)
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp.innerHTML = response.main.temp;
-      temp1.innerHTML = response.main.temp;
-      feels_like.innerHTML = response.main.feels_like;
-      humidity.innerHTML = response.main.humidity;
-      humidity1.innerHTML = response.main.humidity;
-      min_temp.innerHTML = response.main.temp_min;
-      max_temp.innerHTML = response.main.temp_max;
-      wind_speed.innerHTML = response.wind.speed;
-      wind_speed1.innerHTML = response.wind.speed;
-      wind_degrees.innerHTML = response.wind.deg;
+      temp.innerHTML = response.main.temp + " °C";
+      temp1.innerHTML = response.main.temp + " °C";
+      feels_like.innerHTML = response.main.feels_like + " °C";
+      humidity.innerHTML = response.main.humidity + " %";
+      humidity1.innerHTML = response.main.humidity + " %";
+      min_temp.innerHTML = response.main.temp_min + " °C";
+      max_temp.innerHTML = response.main.temp_max + " °C";
+      wind_speed.innerHTML = response.wind.speed + " m/s";
+      wind_speed1.innerHTML = response.wind.speed + " m/s";
+      wind_degrees.innerHTML = response.wind.deg + "°";
       sunset.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -247,20 +247,18 @@ const getWeather = (city) => {
 
   // Mumbai weather
   cityName1.innerHTML = "Mumbai";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=Mumbai&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp2.innerHTML = response.main.temp;
-      feels_like2.innerHTML = response.main.feels_like;
-      humidity2.innerHTML = response.main.humidity;
-      min_temp2.innerHTML = response.main.temp_min;
-      max_temp3.innerHTML = response.main.temp_max;
-      wind_speed2.innerHTML = response.wind.speed;
-      wind_degrees2.innerHTML = response.wind.deg;
+      temp2.innerHTML = response.main.temp + " °C";
+      feels_like2.innerHTML = response.main.feels_like + " °C";
+      humidity2.innerHTML = response.main.humidity + " %";
+      min_temp2.innerHTML = response.main.temp_min + " °C";
+      max_temp3.innerHTML = response.main.temp_max + " °C";
+      wind_speed2.innerHTML = response.wind.speed + " m/s";
+      wind_degrees2.innerHTML = response.wind.deg + "°";
       sunset2.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -268,20 +266,18 @@ const getWeather = (city) => {
 
   // Delhi weather
   cityName2.innerHTML = "New Delhi";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=New Delhi&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=New Delhi&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp3.innerHTML = response.main.temp;
-      feels_like3.innerHTML = response.main.feels_like;
-      humidity3.innerHTML = response.main.humidity;
-      min_temp3.innerHTML = response.main.temp_min;
-      max_temp4.innerHTML = response.main.temp_max;
-      wind_speed3.innerHTML = response.wind.speed;
-      wind_degrees3.innerHTML = response.wind.deg;
+      temp3.innerHTML = response.main.temp + " °C";
+      feels_like3.innerHTML = response.main.feels_like + " °C";
+      humidity3.innerHTML = response.main.humidity + " %";
+      min_temp3.innerHTML = response.main.temp_min + " °C";
+      max_temp4.innerHTML = response.main.temp_max + " °C";
+      wind_speed3.innerHTML = response.wind.speed + " m/s";
+      wind_degrees3.innerHTML = response.wind.deg + "°";
       sunset3.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -289,20 +285,18 @@ const getWeather = (city) => {
 
   // Kolkata weather
   cityName3.innerHTML = "Kolkata";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=Kolkata&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp4.innerHTML = response.main.temp;
-      feels_like4.innerHTML = response.main.feels_like;
-      humidity4.innerHTML = response.main.humidity;
-      min_temp4.innerHTML = response.main.temp_min;
-      max_temp5.innerHTML = response.main.temp_max;
-      wind_speed4.innerHTML = response.wind.speed;
-      wind_degrees4.innerHTML = response.wind.deg;
+      temp4.innerHTML = response.main.temp + " °C";
+      feels_like4.innerHTML = response.main.feels_like + " °C";
+      humidity4.innerHTML = response.main.humidity + " %";
+      min_temp4.innerHTML = response.main.temp_min + " °C";
+      max_temp5.innerHTML = response.main.temp_max + " °C";
+      wind_speed4.innerHTML = response.wind.speed + " m/s";
+      wind_degrees4.innerHTML = response.wind.deg + "°";
       sunset4.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -310,20 +304,18 @@ const getWeather = (city) => {
 
   // Mysore weather
   cityName4.innerHTML = "Mysore";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Mysore&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=Mysore&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp5.innerHTML = response.main.temp;
-      feels_like5.innerHTML = response.main.feels_like;
-      humidity5.innerHTML = response.main.humidity;
-      min_temp5.innerHTML = response.main.temp_min;
-      max_temp6.innerHTML = response.main.temp_max;
-      wind_speed5.innerHTML = response.wind.speed;
-      wind_degrees5.innerHTML = response.wind.deg;
+      temp5.innerHTML = response.main.temp + " °C";
+      feels_like5.innerHTML = response.main.feels_like + " °C";
+      humidity5.innerHTML = response.main.humidity + " %";
+      min_temp5.innerHTML = response.main.temp_min + " °C";
+      max_temp6.innerHTML = response.main.temp_max + " °C";
+      wind_speed5.innerHTML = response.wind.speed + " m/s";
+      wind_degrees5.innerHTML = response.wind.deg + "°";
       sunset5.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -331,20 +323,18 @@ const getWeather = (city) => {
 
   // Pune weather
   cityName5.innerHTML = "Pune";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=Pune&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp6.innerHTML = response.main.temp;
-      feels_like6.innerHTML = response.main.feels_like;
-      humidity6.innerHTML = response.main.humidity;
-      min_temp6.innerHTML = response.main.temp_min;
-      max_temp7.innerHTML = response.main.temp_max;
-      wind_speed6.innerHTML = response.wind.speed;
-      wind_degrees6.innerHTML = response.wind.deg;
+      temp6.innerHTML = response.main.temp + " °C";
+      feels_like6.innerHTML = response.main.feels_like + " °C";
+      humidity6.innerHTML = response.main.humidity + " %";
+      min_temp6.innerHTML = response.main.temp_min + " °C";
+      max_temp7.innerHTML = response.main.temp_max + " °C";
+      wind_speed6.innerHTML = response.wind.speed + " m/s";
+      wind_degrees6.innerHTML = response.wind.deg + "°";
       sunset6.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -352,20 +342,18 @@ const getWeather = (city) => {
 
   // Bangalore weather
   cityName6.innerHTML = "Bangalore";
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=68c8998b8fa0f2a3b5af243607a8a3b3&units=metric`
-  )
+  fetch(`${BASE_URL}?q=Bangalore&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
     .then((response) => {
       console.log(response);
 
-      temp7.innerHTML = response.main.temp;
-      feels_like7.innerHTML = response.main.feels_like;
-      humidity7.innerHTML = response.main.humidity;
-      min_temp7.innerHTML = response.main.temp_min;
-      max_temp8.innerHTML = response.main.temp_max;
-      wind_speed7.innerHTML = response.wind.speed;
-      wind_degrees7.innerHTML = response.wind.deg;
+      temp7.innerHTML = response.main.temp + " °C";
+      feels_like7.innerHTML = response.main.feels_like + " °C";
+      humidity7.innerHTML = response.main.humidity + " %";
+      min_temp7.innerHTML = response.main.temp_min + " °C";
+      max_temp8.innerHTML = response.main.temp_max + " °C";
+      wind_speed7.innerHTML = response.wind.speed + " m/s";
+      wind_degrees7.innerHTML = response.wind.deg + "°";
       sunset7.innerHTML = new Date(response.sys.sunset * 1000).toLocaleTimeString();
     })
     .catch((err) => console.error(err));
@@ -378,7 +366,6 @@ submit.addEventListener("click", (e) => {
 });
 
 getWeather("Shimoga");
-
 
 
 
